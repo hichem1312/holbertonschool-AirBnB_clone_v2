9ff8dd0680efddabcc92edc69759e04d33bde4d7
hichem <hichemmaatoug55@gmail.com>
2022-08-10 10:56:17 -0700

    d

--- models/review.py
+++ models/review.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""Review module for the HBNB project."""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """Review class to store review information."""
+
+    from models import storage_t
+    from models.place import Place
+    from models.user import User
+    if storage_t == 'db':
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey(Place.id), nullable=False)
+        user_id = Column(String(60), ForeignKey(User.id), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize model."""
+        super().__init__(*args, **kwargs)
