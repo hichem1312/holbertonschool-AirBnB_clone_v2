dd28063e864370b979053ad1f9a14a511d9640b6
hichem <hichemmaatoug55@gmail.com>
2022-08-10 09:25:14 -0700

    city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    from models import storage_t
+    from models.state import State
+
+    if storage_t == 'db':
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey(State.id), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("place")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """model"""
+        super().__init__(*args, **kwargs)
