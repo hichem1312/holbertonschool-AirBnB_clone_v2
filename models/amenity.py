59599f1e553ad1fe081792279f7199ead7a6b82f
hichem <hichemmaatoug55@gmail.com>
2022-08-10 10:57:28 -0700

    e

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""State Module for HBNB project."""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+import models
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Class Representing amenities."""
+
+    if models.storage_t == 'db':
+        from models.place import place_amenity
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Amenity."""
+        super().__init__(*args, **kwargs)
